162a6bcb2348397cafe3665987990d689c0aa9a8
Glenn Lanzer <lanzertrey@gmail.com>
2017-09-08 19:14:15 -0500

    Add useBracketNotation...js and md files

---
+++ Javascript-Algorithms-and-Data-Structures/Basic-JavaScript/Use-Bracket-Notation-To-Find-The-Last-Character-In-A-String/useBracketNotationToFindTheLastCharacterInString.js
@@ -0,0 +1,13 @@
+// USE BRACKET NOTATION TO FIND THE LAST CHARACTER IN A STRING
+
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Use 'bracket notation' to find the last character in the `lastName` variable:
+var lastName = "Lovelace";
+var lastLetterOfLastName = lastName[lastName.length - 1]; // The value of `lastLetterOfLastName` is `e`
+
+// Tests:
+// You have to use `.length` to get the last letter.
+console.log(lastLetterOfLastName); // should be "e".
