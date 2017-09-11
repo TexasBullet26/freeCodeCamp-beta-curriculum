4722decdeaa88767143cd371cc75522219e10d1e
Glenn Lanzer <lanzertrey@gmail.com>
2017-09-11 13:38:04 -0500

    Add addingDefualtSwitch..js and .md files

---
+++ Javascript-Algorithms-and-Data-Structures/Basic-JavaScript/Adding_a_Default_Option_in_Switch_Statements/addingDefaultOptionInSwitchStatements.js
@@ -0,0 +1,32 @@
+function switchOfStuff(val)
+{
+  var answer = "";
+
+  switch(val)
+  {
+    case "a":
+      answer = "apple";
+      break;
+
+    case "b":
+      answer = "bird";
+      break;
+
+    case "c":
+      answer = "cat";
+      break;
+
+    default:
+      answer = "stuff";
+      break;
+  }
+
+  return answer;
+
+}
+
+// Tests
+switchOfStuff(1);
+switchOfStuff(2);
+switchOfStuff(3);
+switchOfStuff(4);
