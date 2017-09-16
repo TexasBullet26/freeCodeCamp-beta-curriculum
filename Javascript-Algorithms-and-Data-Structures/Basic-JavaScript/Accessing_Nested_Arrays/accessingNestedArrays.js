0c3e677534706064472f97e9de88c742e5fcf9d7
Glenn Lanzer <lanzertrey@gmail.com>
2017-09-16 13:42:25 -0500

    Add solution to accessingNestedArrays.js file

---
+++ Javascript-Algorithms-and-Data-Structures/Basic-JavaScript/Accessing_Nested_Arrays/accessingNestedArrays.js
@@ -0,0 +1,52 @@
+// Example:
+var ourPets =
+[
+  {
+    animalType: "cat",
+    names:
+    [
+      "Meowzer",
+      "fluffy",
+      "Kit-Cat"
+    ]
+  },
+
+  {
+    animalType: "dog",
+    names:
+    [
+      "Spot",
+      "Bowser",
+      "Frankie"
+    ]
+  }
+];
+
+ourpets[0].names[1]; // "Fluffy"
+ourPets[1].names[0]; // "Spot"
+
+// Solution:
+var myPlants =
+[
+  {
+    type: "flowers",
+    list:
+    [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+
+  {
+    type: "trees",
+    list:
+    [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+var secondTree = myPlants[1].list[1]; // "pine"
