2b1a133b7ad86a1580e60187bde45f90f09c2301
Glenn Lanzer <lanzertrey@gmail.com>
2017-09-19 10:45:43 -0500

    Change syntax for top solution

--- Javascript-Algorithms-and-Data-Structures/Basic-JavaScript/Record_Collection/recordCollection.js
+++ Javascript-Algorithms-and-Data-Structures/Basic-JavaScript/Record_Collection/recordCollection.js
@@ -41,10 +41,35 @@ var collection =
 var collectionCopy = JSON.parse(JSON.stringify(collection));
 
 // Only change code below this line
-function updateRecords(id, prop, value) {
+function updateRecords(id, prop, value)
+{
+  if ( ( prop === "tracks" ) && ( value !== "") )
+  {
 
+    if ( collection[id][prop] )
+    {
+      collection[id][prop].push(value);
+    }
+
+    else
+    {
+      collection[id][prop]=[value];
+    }
+
+  }
+
+  else if ( value !== "" )
+  {
+    collection[id][prop] = value;
+  }
+
+  else
+  {
+    delete collection[id][prop];
+  }
 
   return collection;
+
 }
 
 // Alter values below to test your code
